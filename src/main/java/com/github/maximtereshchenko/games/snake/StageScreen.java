f4c25ec39502ec836107478ccecc912866367af1
maximtereshchenko <tereshchenko.xini@gmail.com>
2026-07-03 18:56:00 +0200

    StageScreen

---
+++ src/main/java/com/github/maximtereshchenko/games/snake/StageScreen.java
@@ -0,0 +1,38 @@
+package com.github.maximtereshchenko.games.snake;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+final class StageScreen extends ScreenAdapter {
+
+    private final Stage stage;
+
+    StageScreen(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+}
