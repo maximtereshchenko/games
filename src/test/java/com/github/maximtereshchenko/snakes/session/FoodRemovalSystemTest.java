67711417c638656529200c0a9676545638b3da3d
maximtereshchenko <tereshchenko.xini@gmail.com>
2026-08-05 07:56:11 +0200

    FoodRemovalSystem

---
+++ src/test/java/com/github/maximtereshchenko/snakes/session/FoodRemovalSystemTest.java
@@ -0,0 +1,52 @@
+package com.github.maximtereshchenko.snakes.session;
+
+import com.github.maximtereshchenko.ecs.Entity;
+import com.github.maximtereshchenko.ecs.Query;
+import com.github.maximtereshchenko.ecs.World;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+final class FoodRemovalSystemTest {
+
+    private final World world = new World();
+    private final Iterable<Entity> foodEntities =
+        world.entities(new Query().all(Food.class));
+    private final FoodRemovalSystem foodRemovalSystem =
+        new FoodRemovalSystem(world);
+
+    @BeforeEach
+    void setUp() {
+        world.addSystems(foodRemovalSystem);
+    }
+
+    @Test
+    void givenNoFoodConsumed_thenFoodNotRemoved() {
+        world.addComponents(world.createEntity(), FoodWarping.INSTANCE);
+        world.addComponents(world.createEntity(), new Food(1));
+        world.update(0);
+        assertThat(foodEntities).hasSize(1);
+    }
+
+    @Test
+    void givenNoFoodWarping_thenFoodNotRemoved() {
+        world.addComponents(world.createEntity(), new FoodConsumed(1));
+        world.addComponents(world.createEntity(), new Food(1));
+        world.update(0);
+        assertThat(foodEntities).hasSize(1);
+    }
+
+    @Test
+    void givenFoodConsumed_thenFoodRemoved() {
+        world.addComponents(
+            world.createEntity(),
+            new FoodConsumed(1),
+            FoodWarping.INSTANCE
+        );
+        world.addComponents(world.createEntity(), new Food(1));
+        world.addComponents(world.createEntity(), new Food(1));
+        world.update(0);
+        assertThat(foodEntities).isEmpty();
+    }
+}
