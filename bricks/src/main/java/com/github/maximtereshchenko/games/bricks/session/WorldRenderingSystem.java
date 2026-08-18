ce42cb0a3e86f8ecbdbc4d5d69738cb56782796f
maximtereshchenko <tereshchenko.xini@gmail.com>
2026-08-18 19:47:37 +0200

    more levels

--- bricks/src/main/java/com/github/maximtereshchenko/games/bricks/session/WorldRenderingSystem.java
+++ bricks/src/main/java/com/github/maximtereshchenko/games/bricks/session/WorldRenderingSystem.java
@@ -38,7 +38,7 @@ final class WorldRenderingSystem implements System {
         viewport.apply();
         shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.getColor().set(Color.valueOf("#22004f"));
+        shapeRenderer.getColor().set(Color.valueOf("#1a1a1a")); //TODO
         shapeRenderer.rect(0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
         for (var entity : entities) {
             var color = entity.component(Color.class);
