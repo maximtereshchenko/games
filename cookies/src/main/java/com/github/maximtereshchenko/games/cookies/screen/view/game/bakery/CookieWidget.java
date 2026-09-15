e36a7f073656c61459223aebf9eb1ed242279ae8
maximtereshchenko <tereshchenko.xini@gmail.com>
2026-09-15 14:37:34 +0200

    CookieWidget active buff background

--- cookies/src/main/java/com/github/maximtereshchenko/games/cookies/screen/view/game/bakery/CookieWidget.java
+++ cookies/src/main/java/com/github/maximtereshchenko/games/cookies/screen/view/game/bakery/CookieWidget.java
@@ -6,11 +6,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Value;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.github.maximtereshchenko.games.cookies.domain.BakeryService;
+import com.github.maximtereshchenko.games.cookies.domain.Buff;
 import com.github.maximtereshchenko.games.cookies.screen.view.game.BigDecimalFormatter;
 
 import java.util.Random;
 
 final class CookieWidget extends Container<Stack> {
+
+    private final Style style;
+    private final BakeryService bakeryService;
 
     CookieWidget(
         Skin skin,
@@ -18,7 +22,8 @@ final class CookieWidget extends Container<Stack> {
         BakeryService bakeryService,
         Random random
     ) {
-        background(skin.get(Style.class).background);
+        this.style = skin.get(Style.class);
+        this.bakeryService = bakeryService;
         size(Value.percentWidth(0.4f, this));
         var flareWidget = new FlareWidget(skin);
         flareWidget.act(FlareWidget.CYCLE_TIME_SECONDS);
@@ -37,8 +42,31 @@ final class CookieWidget extends Container<Stack> {
         setActor(stack);
     }
 
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        setBackground(background());
+    }
+
+    private Drawable background() {
+        if (hasActiveBuff()) {
+            return style.activeBuffBackground;
+        }
+        return style.standardBackground;
+    }
+
+    private boolean hasActiveBuff() {
+        for (var buff : Buff.values()) {
+            if (bakeryService.buffInterval(buff).progress() < 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private static final class Style {
 
-        Drawable background;
+        Drawable standardBackground;
+        Drawable activeBuffBackground;
     }
 }
